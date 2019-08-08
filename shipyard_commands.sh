04d587d8decfc13f2b639233d259908781b764bf
anonymous <s.h.h.j.smit@tudelft.nl>
2019-08-08 19:54:50 +0200

    working version singularity busybox

--- shipyard_commands.sh
+++ shipyard_commands.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #start pool
-python3 /opt/batch-shipyard/shipyard.py pool add --configdir /home/azureuser/Documents/shipyard_containers/shipyard_configdir_singularity/
+python3 /opt/batch-shipyard/shipyard.py pool add --configdir shipyard_configdir_singularity/
 
 #running job
 python3 /opt/batch-shipyard/shipyard.py jobs add --configdir shipyard_configdir_singularity/ --tail stdout.txt
 
 #deleting job
-#python3 /opt/batch-shipyard/shipyard.py jobs del -y --wait
+python3 /opt/batch-shipyard/shipyard.py jobs del --configdir shipyard_configdir_singularity/ -y --wait
 
 #deleting the pool
-#python3 /opt/batch-shipyard/shipyard.py pool del --configdir shipyard_configdir_singularity/ -y
+python3 /opt/batch-shipyard/shipyard.py pool del --configdir shipyard_configdir_singularity/ -y
